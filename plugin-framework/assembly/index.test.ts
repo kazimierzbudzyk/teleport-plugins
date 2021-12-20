a48d4da637b6a7b075b43d9836b3120f0145d353
Victor Sokolov <gzigzigzeo@gmail.com>
2021-12-20 17:05:55 +0300

    Test helper

--- plugin-framework/assembly/index.test.ts
+++ plugin-framework/assembly/index.test.ts
@@ -1,11 +1,11 @@
-import { getFixture } from './vendor/test';
+import { getFixture, arrayToDataView } from './vendor/test';
 import { handleEvent } from '.';
 import { events } from './vendor/teleport';
 
 export { __protobuf_alloc, __protobuf_free, __protobuf_getAddr, __protobuf_getLength, __protobuf_setu8 } from './vendor/teleport';
 export { handleEvent } from './index';
 
-export function test():void {
+export function test(): void {
     trace("teleport-plugin-framework tests")
 
     testRegularEvent();
@@ -15,33 +15,28 @@ export function test():void {
     trace("Success!")
 }
 
-function testRegularEvent():void {
+function testRegularEvent(): void {
     const createMeData = getFixture(1)
     assert(handleEvent(createMeData) != null, "Regular event is handled")
 }
 
-function testSkipLoginSecretSanta():void {
+function testSkipLoginSecretSanta(): void {
     const loginSecretSantaData = getFixture(2)
     assert(handleEvent(loginSecretSantaData) == null, "Skip secret-santa login")
 }
 
-function testAddAnnotations():void {
+function testAddAnnotations(): void {
     const testAccessRequest = getFixture(3)
     const result = handleEvent(testAccessRequest)
     assert(result != null, "Request is processed")
 
-    // NOTE: This block will be wrapped into test helper || decode/encode signatures will operate StaticArray
-    const a = result as Array<u8>
-    const v = new DataView(new ArrayBuffer(a.length));
-    for (let i:i32 = 0; i < a.length; i++) {
-        v.setUint8(i, a.at(i))
-    }
-    // NOTE: -----
-
-    const changedEvent = events.OneOf.decode(v);
+    const changedEvent = events.OneOf.decode(arrayToDataView(result as Array<u8>));
     assert(changedEvent.AccessRequestCreate != null, "AccessRequestCreate is present")
 
     const changedAccessRequest = changedEvent.AccessRequestCreate as events.AccessRequestCreate;
-    assert(changedAccessRequest.Annotations.fields.get("seen-by-us").string_value == "yes", "changed-by-us annotation is set")
+    assert(
+        changedAccessRequest.Annotations.fields.get("seen-by-us").string_value == "yes", 
+        "seen-by-us annotation is set"
+    )
 }
 
--- plugin-framework/assembly/vendor/test.ts
+++ plugin-framework/assembly/vendor/test.ts
@@ -1,1 +1,10 @@
 export declare function getFixture(n: i32): DataView;
+
+// Helper method, TODO: think of using same sigs on encode/decode
+export function arrayToDataView(source: Array<u8>): DataView {
+    const v = new DataView(new ArrayBuffer(source.length));
+    for (let i: i32 = 0; i < source.length; i++) {
+        v.setUint8(i, source.at(i))
+    }
+    return v;
+}
